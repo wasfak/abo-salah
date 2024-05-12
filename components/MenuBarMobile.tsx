260ce9a67eabf84aa9f84fe1f264ac4a0f4263b3
wasfak <hardmodz@yahoo.com>
2024-05-12 18:46:05 +0300

    after toast

--- components/MenuBarMobile.tsx
+++ components/MenuBarMobile.tsx
@@ -4,7 +4,11 @@ import Link from "next/link";
 import { FiMenu as Icon } from "react-icons/fi";
 import { FaUser } from "react-icons/fa";
 
-export default function MenuBarMobile({ setter }) {
+type SideBarProps = {
+  setter: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function MenuBarMobile({ setter }: SideBarProps) {
   return (
     <nav className="md:hidden z-20 fixed top-0 left-0 right-0 h-[60px] bg-black flex [&>*]:my-auto px-2">
       <button
